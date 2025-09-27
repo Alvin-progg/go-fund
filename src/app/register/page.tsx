c981fdf4653ed66eadb9528e7a59312b798553f9
ALvin-progg <doctoralvin726@gmail.com>
2025-09-27 14:14:26 +0800

    21s

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-    <section>
-        
+    <section className=''>
+      <form >
+        <h1>Create Account</h1>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+      </form>
     </section>
   )
 }
